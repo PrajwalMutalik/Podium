7248f4e8d07eae478545494dc40d534b2dfaa0d0
PrajwalMutalik <prajju.mutalik@gmail.com>
2025-09-09 10:04:14 +0530

    login issue

--- server/index.js
+++ server/index.js
@@ -47,9 +47,9 @@ app.use('/api/leaderboard', require('./routes/leaderboard'));
 
 // --- Serve Static Frontend ---
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
   });
 }
 
